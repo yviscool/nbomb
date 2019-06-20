9494a7cbc54a8ddee5b8bdb172d12df076de8237
Y.V <5196666qwe@gmail.com>
2019-06-20 14:39:08 +0800

    delete all ts files

--- packages/web/src/common/index.ts
+++
@@ -1,4 +0,0 @@
-'use strict';
-export * from './parse-int.pipe';
-export * from './validation.pipe';
-export * from './class-serializer.interceptor';
--- packages/web/src/mixin/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./context_creator";
-export * from "./exception_handler";
-export * from "./router_execution_context";
-export * from "./router_param_factory";
-export * from "./router_proxy";
